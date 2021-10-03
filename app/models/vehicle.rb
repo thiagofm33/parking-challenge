32d8bb81defda959f5cd03c7f0116dfb5f687b71
Thiago Martins <thiagofm33@gmail.com>
2021-10-03 08:58:30 -0300

    Added uniqueness validation for vehicle plates

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,5 +3,8 @@ class Vehicle < ApplicationRecord
 
   enum vehicle_type: [ :car, :motorcycle ], _default: 0
 
-  validates :plate, presence: true, format: { with: /\A[A-Z]{3}\-\d{4}\z/ }
+  validates :plate,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[A-Z]{3}\-\d{4}\z/ }
 end
